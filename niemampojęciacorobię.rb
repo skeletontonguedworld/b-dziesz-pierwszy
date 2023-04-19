f99a5f518fc9824b7f1237aa19e31ae8fcfdbf23
skeletontonguedworld <129410383+skeletontonguedworld@users.noreply.github.com>
2023-04-19 18:48:40 +0200

    Delete niemampojęciacorobię.rb

--- niemampojęciacorobię.rb
+++
@@ -1,9 +0,0 @@
-puts "czy kurwa przestaną wiercić"
-puts "XDDDD"
-puts "czy mi to kurwa wyświetli?"
-Kari_ma_dośc = gets
-if Kari_ma_dośc.include? "Ka"
-  puts "Kari, idź zapalić"
-else "Nawet, jak nie jesteś Kari, to możesz zapalić"
-end
-Kari_ma_dośc_2 = gets
